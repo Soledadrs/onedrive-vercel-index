aaeb0dc09d776696839c9a3924a4544989e1f513
Arley <77773409+arleycn@users.noreply.github.com>
2022-03-15 19:41:17 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -14,7 +14,7 @@ module.exports = {
   icon: '/icons/1500.png',
 
   // The name of your website. Present alongside your icon.
-  title: "Arley's OneDrive",
+  title: "Arley's Drive",
 
   // The folder that you are to share publicly with onedrive-vercel-index. Use '/' if you want to share your root folder.
   baseDirectory: '/',
